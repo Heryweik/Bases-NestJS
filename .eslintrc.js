eab0c1f3bbdf441db6e329eebf5355dd638380eb
Yhonny Aplicano <yhonny296@gmail.com>
2024-07-31 19:10:53 -0600

    [Fix] - Solucion de "Delete '␍' eslint(prettier/prettier)"

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,11 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     // "prettier/prettier": 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
